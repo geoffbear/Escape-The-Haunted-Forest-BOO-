a3c2a332fa5a8beeb51ba1c187056f1d7925c1b6
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-06-12 13:19:20 -0400

    changes

--- Form5.cs
+++ Form5.cs
@@ -19,8 +19,8 @@ namespace Escape_The_Haunted_Forest__BOO__
         }
 
         private void globeGameForm_Load(object sender, EventArgs e)
-        {
-            //SoundPlayer globeGameMusic = new SoundPlayer(Properties.);
+        { 
+            SoundPlayer globeGameMusic = new SoundPlayer(Properties.Resources.globeGameMusic1);
         }
     }
 }
